6cb7ebb2a56848fb17206e0ee3a44142066c02e1
KolchakovDanil <danyakisliy@mail.ru>
2023-10-29 19:54:23 +0500

    Task5

---
+++ Tyuiu.KolchakovDR.Sprint3.Task4.V6.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.KolchakovDR.Sprint3.Task4.V6.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.KolchakovDR.Sprint3.Task4.V6.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("67d0ea16-ee32-4c3d-884d-8538d68018f5")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.KolchakovDR.Sprint3.Task4.V6/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.KolchakovDR.Sprint3.Task4.V6.Lib;
+using TaskHeaderGenerator;
+
+namespace Tyuiu.KolchakovDR.Sprint3.Task4.V6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TaskHG thg = new TaskHG(
+                3,
+                "Колчаков Д. Р.",
+                "ИИПб-23-2",
+                "Использование операторов continue и break в циклах",
+                4,
+                6,
+                "На отрезке, где x принимает значения от -5 до 5, вычислить значение функции y=x/(cos(x)-sin(x)). При х = 0 прервать цикл.Полученные значения суммировать.");
+            thg.printHeader();
+
+            DataService ds = new DataService();
+
+            int startValue = -5;
+            int stopValue = 5;
+
+            Console.WriteLine("Старт шага = " + startValue);
+
+            Console.WriteLine("Конец шага = " + stopValue);
+
+            thg.printFooter();
+            Console.WriteLine("Сумма ряда = " + ds.Calculate(startValue, stopValue));
+            Console.ReadKey();
+        }
+    }
+}
